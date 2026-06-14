0324e0b4369c9c3b04cc6de17c3369133b137dcf
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-14 16:03:09 +0530

    fix: add cors to backend for frontend connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cookiePraser = require("cookie-parser");
-
+const cors = require("cors");
 
 const authRouter = require("./routes/auth.routes");
 const accountRouter = require("./routes/accounts.routes")
@@ -8,6 +8,14 @@ const transactionRoutes = require("./routes/transaction.routes")
 
 
 const app = express();
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow all origins for the pitch presentation
+    callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookiePraser());
