1be9a9c0f73290c7fd5867931ff79d1b91fcefb0
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-30 03:58:23 +0530

    fixed again something

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
--- backend/src/services/email.services.js
+++ backend/src/services/email.services.js
@@ -1,9 +1,5 @@
 const nodemailer = require('nodemailer');
 
-const dns = require("dns");
-
-dns.setDefaultResultOrder("ipv4first");
-
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
