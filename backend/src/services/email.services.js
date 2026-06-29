bf62487560d6f866f7b5d6d3cccf9499fe6b5399
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-30 03:48:59 +0530

    fixed a bug

--- backend/src/services/email.services.js
+++ backend/src/services/email.services.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
-import dns from "node:dns";
+const dns = require("dns");
+
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
